c51e1311368c2e97154b2d68863c0ae148b17ed3
SharatChandra23 <sarat.maganti23@gmail.com>
2022-07-11 11:57:08 +0530

    url changes

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -21,7 +21,7 @@ function booksController(Book) {
         const returnBooks = books.map((book) => {
           const newBook = book.toJSON();
           newBook.links = {};
-          newBook.links.self = `http://${req.headers.host}/api/books/${book._id}`;
+          newBook.links.self = `https://${req.headers.host}/api/books/${book._id}`;
           return newBook;
         });
         return res.json(returnBooks);
